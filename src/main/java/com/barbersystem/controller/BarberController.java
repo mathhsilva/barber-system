95bc147383c59cdebdc00a1ab47ccd5f0454f898
Matheus Cordeiro <matheus.cordeiro@rpinfo.com.br>
2024-08-05 12:00:51 -0300

    Commit Initial

---
+++ src/main/java/com/barbersystem/controller/BarberController.java
@@ -0,0 +1,4 @@
+package com.barbersystem.controller;
+
+public class BarberController {
+}
---
+++ src/test/java/com/barbersystem/BarberSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
