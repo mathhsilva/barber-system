5b09e10ba1c6e0a5a23293f555d479d7ebb73118
Matheus Cordeiro <matheus.cordeiro@rpinfo.com.br>
2024-08-06 13:43:18 -0300

    Ajuste dos Services

--- service/BarberService.java
+++ service/BarberService.java
@@ -1,4 +1,26 @@
 package com.barbersystem.service;
 
+import com.barbersystem.model.Barber;
+import com.barbersystem.repository.BarberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BarberService {
+    @Autowired
+    private BarberRepository barberRepository;
+
+    public List<Barber> getAllBarbers() {
+        return barberRepository.findAll();
+    }
+
+    public Barber getBarberById(Long id) {
+        return barberRepository.findById(id).orElse(null);
+    }
+
+    public Barber saveBarber(Barber barber) {
+        return barberRepository.save(barber);
+    }
 }
