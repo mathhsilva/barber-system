f4d36b46aeebd2a7222e44b4f71c40ea58e35814
Matheus Cordeiro <matheus.cordeiro@rpinfo.com.br>
2024-08-06 13:40:08 -0300

    Ajuste dos controllers

--- controller/CutController.java
+++ controller/CutController.java
@@ -1,7 +1,31 @@
 package com.barbersystem.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.barbersystem.model.Cut;
+import com.barbersystem.service.CutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/cuts")
 public class CutController {
+    @Autowired
+    private CutService cutService;
+
+    @GetMapping("/barber/{barberId}")
+    public List<Cut> getCutsByBarberAndDate(@PathVariable Long barberId, @RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
+        return cutService.getCutsByBarberAndDate(barberId, startDate, endDate);
+    }
+
+    @GetMapping
+    public List<Cut> getAllCutsByDate(@RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
+        return cutService.getAllCutsByDate(startDate, endDate);
+    }
+
+    @PostMapping
+    public Cut saveCut(@RequestBody Cut cut) {
+        return cutService.saveCut(cut);
+    }
 }
