979b2ea7f64ef016d54b63e6d221df2d284a213a
Matheus Cordeiro <matheus.cordeiro@rpinfo.com.br>
2024-08-06 14:02:10 -0300

    Exclusão de pastas duplicadas

--- repository/BarberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.barbersystem.repository;
-
-import com.barbersystem.model.Barber;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BarberRepository extends JpaRepository<Barber, Long> {
-
-}
--- repository/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.barbersystem.repository;
-
-import com.barbersystem.model.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<Service, Long> {
-
-}
